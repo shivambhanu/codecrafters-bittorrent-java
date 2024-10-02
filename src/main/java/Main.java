262ae8e1523fca89b150fdabbfc06c484a5525fd
Shivam Bhanu <shivambhanu6924@gmail.com>
2024-10-02 09:01:03 +0530

    String to Object data type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
     if("decode".equals(command)) {
       //  Uncomment this block to pass the first stage
         String bencodedValue = args[1];
-        String decoded;
+        Object decoded;
         try {
           decoded = decodeBencode(bencodedValue);
         } catch(RuntimeException e) {
@@ -26,7 +26,7 @@ public class Main {
 
   }
 
-  static String decodeBencode(String bencodedString) {
+  static Object decodeBencode(String bencodedString) {
     if (Character.isDigit(bencodedString.charAt(0))) {
       int firstColonIndex = 0;
       for(int i = 0; i < bencodedString.length(); i++) { 
@@ -37,11 +37,9 @@ public class Main {
       }
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
       return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-
     } else if(bencodedString.charAt(0) == 'i'){
       int len = bencodedString.length();
-      return bencodedString.substring(1, len-1);
-
+      return Integer.parseInt(bencodedString.substring(1, len-1));
     } else {
       throw new RuntimeException("Only strings are supported at the moment");
     }
